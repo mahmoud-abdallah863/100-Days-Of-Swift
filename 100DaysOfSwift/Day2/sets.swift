653fb412e12ca839b03d7046ba85433cff8b272f
Mahmoud Abdallah <mabdallah863@gmail.com>
2021-05-06 03:28:42 +0300

    Finished sets

--- 100DaysOfSwift/Day2/sets.swift
+++ 100DaysOfSwift/Day2/sets.swift
@@ -1,22 +1,66 @@
-////
-////  sets.swift
-////  100DaysOfSwift
-////
-////  Created by Mahmoud Abdallah on 4/7/21.
-////
 //
-//var nums = Set([1, 2, 3, 4])
+//  sets.swift
+//  100DaysOfSwift
 //
-//print("nums = \(nums)")
+//  Created by Mahmoud Abdallah on 4/7/21.
 //
-//nums.insert(5)
-//print("nums = \(nums)")
-//
-//var capacity = nums.capacity
-//print("capacity = \(capacity)")
-//
-//
-//let first_element_ = nums.popFirst() ?? 1
-//print("first element = \(first_element_)")
-//
-//print("numbers after removing the first element=  \(nums)")
+
+func setsMain() {
+    
+    /**
+                Notes:
+            Values order in the set may change every execution time
+     */
+    
+    // How to create empty set
+    let emptySet = Set<Int>()
+    print("Empty set capacity = \(emptySet.capacity)")
+    
+    
+    // Check if set is empty
+    print("Is empty set <emptySet> = \(emptySet.isEmpty)")
+
+    
+    var nums = Set([1, 2, 3, 4])
+
+    print("nums = \(nums)")
+
+    nums.insert(5)
+    print("nums = \(nums)")
+
+    // Capacity: The total number of elements that the set can contain without allocating new storage.
+    //          values: 0, 1, 3, 6, 12, ... (multiple of 3)
+    print("capacity = \(nums.capacity) | count = \(nums.count)")
+
+    let first_element = nums.popFirst() ?? 1
+    print("first element = \(first_element)")
+    
+    print("numbers after removing the first element=  \(nums)")
+    
+    // Remove will return nil if element does not exists
+    let x = nums.remove(1)
+    print("Removed element: \(String(describing: x))")
+    print("nums after removing element \(String(describing: x)): \(nums)")
+    
+    
+    // Check if element exists in set
+    var exists = nums.contains(3)
+    print("Number 3 exists in nums: \(exists)")
+    
+    exists = nums.contains(10)
+    print("Number 10 exists in nums: \(exists)")
+    
+    
+    print("Sorted set = \(nums.sorted())")
+    
+    
+    // Sum the elements in set
+    let sumElement = nums.reduce(0, +)
+    print("nums: \(nums)")
+    print("Sum of elements in nums: \(sumElement)")
+    
+    
+
+    
+}
+setsMain()
